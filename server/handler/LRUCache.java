871d4fb9f2c659e956b897798c7fb307d9ba3d58
Lijun <myutou@163.com>
2018-09-19 14:49:50 -0400

    Create LRUCache.java
    Feature: Support a fixed sized data store with an eviction policy, LRU policy

---
+++ server/handler/LRUCache.java
@@ -0,0 +1,96 @@
+package handler;
+
+import java.util.HashMap;
+
+public class LRUCache<KeyType, ValueType> {
+    private HashMap<KeyType, DLNode> map = new HashMap<KeyType, DLNode>();
+    private DLNode head;
+    private DLNode end;
+    private int capacity;
+    private int len;
+
+    public LRUCache(int capacity) {
+	this.capacity = capacity;
+	len = 0;
+    }
+
+    public ValueType get(KeyType key) {
+	if (map.containsKey(key)) {
+	    DLNode latest = map.get(key);
+	    removeNode(latest);
+	    setHead(latest);
+	    return latest.val;
+	} else {
+	    return null;
+	}
+    }
+
+    public void removeNode(DLNode node) {
+	DLNode cur = node;
+	DLNode pre = cur.pre;
+	DLNode post = cur.next;
+
+	if (pre != null) {
+	    pre.next = post;
+	} else {
+	    head = post;
+	}
+
+	if (post != null) {
+	    post.pre = pre;
+	} else {
+	    end = pre;
+	}
+    }
+
+    public void setHead(DLNode node) {
+	node.next = head;
+	node.pre = null;
+	if (head != null) {
+	    head.pre = node;
+	}
+
+	head = node;
+	if (end == null) {
+	    end = node;
+	}
+    }
+
+    public void set(KeyType key, ValueType value) {
+	if (map.containsKey(key)) {
+	    DLNode oldNode = map.get(key);
+	    oldNode.val = value;
+	    removeNode(oldNode);
+	    setHead(oldNode);
+	} else {
+	    DLNode newNode = new DLNode(key, value);
+	    if (len < capacity) {
+		setHead(newNode);
+		map.put(key, newNode);
+		len++;
+	    } else {
+		map.remove(end.key);
+		end = end.pre;
+		if (end != null) {
+		    end.next = null;
+		}
+
+		setHead(newNode);
+		map.put(key, newNode);
+	    }
+	}
+    }
+
+    class DLNode {
+	public KeyType key;
+	public ValueType val;
+
+	public DLNode pre;
+	public DLNode next;
+
+	public DLNode(KeyType key, ValueType value) {
+	    val = value;
+	    this.key = key;
+	}
+    }
+}
